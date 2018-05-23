586ae3bf302d02d7781dc7b331cc9821554e78c6
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-23 20:03:32 +0300

    new

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,31 @@
+import getpass
 
 
-def get_password_strength(password):
-    pass
+def passwords_mark(password):
+    mark = 0
+    if len(password)>=8:
+        mark+=2
+    if any(word.isupper() for word in password):
+        mark+=2
+    if any(word.islower() for word in password):
+        mark+=2
+    if any(word.isdigit() for word in password):
+        mark+=2
+    if any(["!","@","#","$","%","^","&","*","(",")"] for i in password):
+        mark+=2
+    return mark
 
 
-if __name__ == '__main__':
-    pass
+def print_mark(password_strength, password_mark):
+    print(password_strength, password_mark)
+
+
+def get_password():
+    password = getpass.getpass("Введите пароль: ")
+    return password
+
+
+if __name__ == "__main__":
+    password = get_password()
+    password_mark = passwords_mark(password)
+    print_mark("Сложность пароля: ", password_mark)
